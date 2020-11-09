56597877a23206580ea66cd0cbc37b5e2cef411a
zyy <18716833534@163.com>
2020-11-09 10:27:51 +0800

    update

--- src/leetcode/tree/_124.kt
+++ src/leetcode/tree/_124.kt
@@ -7,25 +7,16 @@ import kotlin.math.max
 object _124 {
     var maxSum = Integer.MIN_VALUE;
     fun maxPathSum(root: TreeNode?): Int {
-        helper(root, Int.MIN_VALUE, false)
+        helper(root)
         return maxSum
     }
 
-    fun helper(root: TreeNode?, sum: Int, isLeafBefore: Boolean): Int {
+    fun helper(root: TreeNode?): Int {
         if (root == null) {
-            return if (isLeafBefore) return Int.MIN_VALUE else 0
+            return 0
         }
-        val leftSum = helper(root.left, max(sum, sum + root.`val`), root.isLeaf() || root.`val` < 0)
-        val rightSum = helper(root.right, max(sum, sum + root.`val`), root.isLeaf() || root.`val` < 0)
-        if (leftSum == Int.MIN_VALUE && rightSum == Int.MIN_VALUE) {
-            return root.`val`.also { maxSum = max(maxSum, it) }
-        }
-        if (leftSum == Int.MIN_VALUE) {
-            return rightSum + root.`val`.also { maxSum = max(max(rightSum, root.`val`), max(maxSum, it)) }
-        }
-        if (rightSum == Int.MIN_VALUE) {
-            return leftSum + root.`val`.also { maxSum = max(max(leftSum, root.`val`), max(maxSum, it)) }
-        }
+        val leftSum = helper(root.left)
+        val rightSum = helper(root.right)
         return max(root.`val`, max(leftSum + root.`val`, rightSum + root.`val`)).also { maxSum = max(leftSum + rightSum + root.`val`, max(maxSum, it)) }
     }
 
@@ -33,5 +24,5 @@ object _124 {
 }
 
 fun main() {
-    _124.maxPathSum(TreeMainClass.stringToTreeNode("[9,-3,0,3,null,null,null,null,-8]"))
+    println(_124.maxPathSum(TreeMainClass.stringToTreeNode("[-1, 2]")))
 }
